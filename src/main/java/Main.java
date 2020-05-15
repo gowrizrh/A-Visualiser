307f149e561aa83c19b1a37e298e2cd2659d7ff6
Gowri <gowrizrh@gmail.com>
2020-05-16 02:32:28 +0930

    Set up Swing window with Canvas!
    So much research went into this...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        Window w = new Window();
-        w.run();
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {}
+
+        RenderWindow frame = new RenderWindow("Starvis");
+        frame.render();
     }
 }
